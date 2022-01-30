7c2975c2f23275cb11aacedbeef97f5e805bf0b7
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-01-30 18:03:16 -0500

    Create cookie clicker.bat

---
+++ cookie clicker.bat
@@ -0,0 +1,38 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: title
+title Cookie Clicker Remasterd (OwO)
+
+:: username 
+color 5a
+set /p user="enter username: "
+
+:: score
+set /A thing=0
+set /A score=score.txt
+
+for /f "tokens=*" %%x in (%score%) do (
+    set /a thing+=1
+    set var[!thing!]=%%x
+)
+
+set /A count=%var[1]%
+
+:: loop func
+:loop
+  color 5a
+  echo Cookie Clicker
+  echo %user%'s score: %count%
+  pause
+  set /A count=%count%+1
+  echo %count% > %score%
+  cls
+  color a5
+  echo Cookie Clicker
+  echo %user%'s score: %count%
+  pause
+  set /A count=%count%+1
+  echo %count% > %score%
+  cls
+goto loop
